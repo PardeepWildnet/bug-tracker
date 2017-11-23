5dbeaade94d6459ef080990af2639d9bde7cc15b
PardeepWildnet <pardeep.wildnet@gmail.com>
2017-11-23 11:20:16 +0530

    remove unwanted files

--- src/extra folders/Quiz/data/quiz/action.js
+++
@@ -1,8 +0,0 @@
-export const FETCH_QUESTIONS = 'Quiz/data/FETCH_QUESTIONS';
-
-export const fetchQuestions = (payload) => {
-	return {
-		type : FETCH_QUESTIONS,
-		payload
-	}
-}
--- src/extra folders/Quiz/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import QuizView from './components/QuizView';
-import './style.css';
-
-const Quiz = () => 
-	<QuizView />
-
-export default Quiz;
